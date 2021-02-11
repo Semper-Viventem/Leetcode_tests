73bcc11fd58977ce27ad0c120d8c37adff4f79bb
Constantine <kostyaxxx8@gmail.com>
2021-02-11 20:28:16 +0300

    Implement another one solution for https://leetcode.com/problems/zigzag-conversion

--- src/main/kotlin/ZigZagConversion.kt
+++ src/main/kotlin/ZigZagConversion.kt
@@ -16,6 +16,23 @@ fun main() {
 }
 
 fun convert(s: String, numRows: Int): String {
+    if (numRows == 1) return  s
+
+    val rows = Array(Math.min(numRows, s.length)) { StringBuilder() }
+
+    var curRow = 0
+    var goingDown = false
+
+    for (c in s) {
+        rows[curRow].append(c)
+        if (curRow == 0 || curRow == numRows - 1) goingDown = !goingDown
+        curRow += if (goingDown) 1 else -1
+    }
+
+    return buildString { rows.forEach { append(it) } }
+}
+
+fun convert1(s: String, numRows: Int): String {
     if (numRows == 1) return s
 
     val diagonalLength = maxOf(numRows - 2, 0)
