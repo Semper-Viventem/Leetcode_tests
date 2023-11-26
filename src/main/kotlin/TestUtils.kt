d60ab75526eb7c4805a048417cfc338ea8ddba3d
Semper-Viventem <kostyaxxx8@gmail.com>
2023-11-26 20:20:49 +0200

    Submit StringToIntegerAtoi solution

--- src/main/kotlin/TestUtils.kt
+++ src/main/kotlin/TestUtils.kt
@@ -1,4 +1,5 @@
 fun <A, B> test(testData: List<Pair<A, B>>, testFunctionExecution: (input: A) -> B) {
+    var isAllPassed = true
     testData.forEach { (test, expected) ->
         if (test is IntArray) {
             println(test.toList())
@@ -6,7 +7,14 @@ fun <A, B> test(testData: List<Pair<A, B>>, testFunctionExecution: (input: A) ->
             println(test)
         }
         val result = testFunctionExecution.invoke(test)
-        println(if (expected == result) "SUCCESS" else "FAILED (output: $result expected: $expected)")
+        val isPassed = expected == result
+        if (!isPassed) {
+            isAllPassed = false
+        }
+        println(if (isPassed) "SUCCESS" else "FAILED (output: $result expected: $expected)")
         println()
     }
+
+    println(if (isAllPassed) "TOTAL: SUCCESS" else "TOTAL: FAILED")
+    println()
 }
