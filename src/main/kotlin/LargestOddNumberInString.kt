f6d9244c6df365801cf338759a064dd445ce1094
Semper-Viventem <kostyaxxx8@gmail.com>
2023-12-07 12:25:37 +0200

    Submit LargestOddNumberInString.kt

---
+++ src/main/kotlin/LargestOddNumberInString.kt
@@ -0,0 +1,45 @@
+/**
+ * You are given a string num, representing a large integer. Return the largest-valued odd integer (as a string) that is a non-empty substring of num, or an empty string "" if no odd integer exists.
+ *
+ * A substring is a contiguous sequence of characters within a string.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: num = "52"
+ * Output: "5"
+ * Explanation: The only non-empty substrings are "5", "2", and "52". "5" is the only odd number.
+ *
+ * Example 2:
+ *
+ * Input: num = "4206"
+ * Output: ""
+ * Explanation: There are no odd numbers in "4206".
+ *
+ * Example 3:
+ *
+ * Input: num = "35427"
+ * Output: "35427"
+ * Explanation: "35427" is already an odd number.
+ *
+ * https://leetcode.com/problems/largest-odd-number-in-string/?envType=daily-question&envId=2023-12-07
+ */
+fun main() = test(
+    testData = listOf(
+        "52" to "5",
+        "4206" to "",
+        "35427" to "35427",
+    ),
+    testFunctionExecution = ::largestOddNumber
+)
+
+fun largestOddNumber(num: String): String {
+    for (i in num.lastIndex downTo 0) {
+        val char = num[i]
+        if (char.digitToInt() % 2 != 0) {
+            return num.substring(0..i)
+        }
+    }
+    return ""
+}
